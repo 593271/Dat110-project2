0e0504ccda41eed31e9a165cf55044cddd71b7dc
593271 <69844338+593271@users.noreply.github.com>
2022-03-03 21:02:42 +0100

    Add files via upload

---
+++ dat110-project2-startcode/src/no/hvl/dat110/iotsystem/TemperatureDevice.java
@@ -0,0 +1,47 @@
+package no.hvl.dat110.iotsystem;
+
+import no.hvl.dat110.client.Client;
+
+import no.hvl.dat110.common.TODO;
+
+public class TemperatureDevice {
+
+	private static final int COUNT = 10;
+
+	public static void main(String[] args) {
+
+		// simulated / virtual temperature sensor
+		TemperatureSensor sn = new TemperatureSensor();
+
+		// TODO - start
+
+		// create a client object and use it to
+		// - connect to the broker - user "sensor" as the user name
+		// - publish the temperature(s)
+		// - disconnect from the broker
+		Client sensor = new Client("sensor", Common.BROKERHOST, Common.BROKERPORT);
+		sensor.connect();
+		for (int i = 0; i < COUNT; i++) {
+			
+			int temp = sn.read();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println(temp);
+			
+			sensor.publish(Common.TEMPTOPIC, Integer.toString(temp));
+		}
+		
+		sensor.disconnect();
+		
+
+		// TODO - end
+
+		System.out.println("Temperature device stopping ... ");
+
+
+	}
+}
---
+++ dat110-project2-startcode/src/no/hvl/dat110/messages/PublishMsg.java
@@ -0,0 +1,44 @@
+package no.hvl.dat110.messages;
+
+import no.hvl.dat110.common.TODO;
+
+public class PublishMsg extends Message {
+
+	// message sent from client to create publish a message on a topic
+
+	// TODO:
+	// Implement object variables - a topic and a message is required
+
+	// Constructor, get/set-methods, and toString method
+	// as described in the project text
+
+	private String topic;
+	private String message;
+
+	public PublishMsg(String user, String topic,String message) {
+		super(MessageType.PUBLISH, user);
+		this.topic = topic;
+		this.message=message;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	@Override
+	public String toString() {
+		return "PublishMsg [topic=" + topic + ", toString()=" + super.toString() + message + "]";
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
